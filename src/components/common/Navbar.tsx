a8c5f31467a78b1e47af6b471572bdf8404e14c0
g.ilhuicatzi <g.ilhuicatzi@gmail.com>
2025-06-29 21:50:10 -0600

    feat: implement Navbar component with navigation links and theme toggle

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,28 @@
+
+import Link from "next/link"
+import { ModeToggle } from "./ModeToggle"
+
+function Navbar() {
+    return (
+        <nav className="flex items-center justify-between p-4">
+            <div>
+                <h1 className="text-2xl font-bold">
+                    <Link href="/">Git</Link>
+                </h1>
+            </div>
+            <div className="flex items-center space-x-4">
+                <ul className="flex space-x-4">
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="#">About</Link>
+                    </li>
+                </ul>
+                <ModeToggle />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
